3e3bae8df60e84b02dabfe8ddb8d1ef27d6b67a1
Bo Hu <tyranthu@hotmail.com>
2019-02-11 07:48:04 +1100

    cleanup and compress

---
+++ precache-manifest.47ef512a74266bf705e879fce59760f7.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc82b8b2e5b9d551e85644a44433868",
+    "url": "/static/media/24.4dc82b8b.png"
+  },
+  {
+    "revision": "fc952894118b06efed48",
+    "url": "/static/css/main.16fddde6.chunk.css"
+  },
+  {
+    "revision": "3c56191a09bb491d290c99a19f38f53b",
+    "url": "/static/media/foo.3c56191a.otf"
+  },
+  {
+    "revision": "a58dd9d7fb4d4497fdf4",
+    "url": "/static/js/1.a58dd9d7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c63abfd622cf6c5e469df2e4b85fa245",
+    "url": "/static/media/soulstone.c63abfd6.png"
+  },
+  {
+    "revision": "8ca7818dbced0ede613f1585e14e6cef",
+    "url": "/static/media/scroll.8ca7818d.png"
+  },
+  {
+    "revision": "0ccceaff1739df0b9093fee4f05a95d2",
+    "url": "/static/media/dragonwarrior.0ccceaff.png"
+  },
+  {
+    "revision": "2e406677d4a79ac7d7aee3d6eaa99b2e",
+    "url": "/static/media/tron-logo.2e406677.png"
+  },
+  {
+    "revision": "7d8e30848b8278c22b32e2bad7c4fbb5",
+    "url": "/static/media/ss-spite.7d8e3084.png"
+  },
+  {
+    "revision": "08a5f40eb93b583c1d8df2ae451e3ffe",
+    "url": "/static/media/weapon-armor-sprite.08a5f40e.png"
+  },
+  {
+    "revision": "ee31d4fb499dfc8ae42e2ef3319a94c3",
+    "url": "/static/media/minion-sprite.ee31d4fb.png"
+  },
+  {
+    "revision": "90bdf80be4af6f7436de1a97c379e74e",
+    "url": "/static/media/auction.90bdf80b.png"
+  },
+  {
+    "revision": "64677ac82a6ef0937f3329832d5ea939",
+    "url": "/static/media/instance-sprite.64677ac8.png"
+  },
+  {
+    "revision": "af36fcaab773487468eab974342172ee",
+    "url": "/static/media/raid-sprite.af36fcaa.png"
+  },
+  {
+    "revision": "96acce2c6a40c7edb60281430a19da36",
+    "url": "/static/media/pvp-sprite.96acce2c.png"
+  },
+  {
+    "revision": "f3e5ae9bf81b5a0a35caf58f21f0cb9c",
+    "url": "/static/media/battleground.f3e5ae9b.png"
+  },
+  {
+    "revision": "7a77cf4c9155a8ca6c58ac0bbd8c4e6d",
+    "url": "/static/media/blacksmith-sprite.7a77cf4c.png"
+  },
+  {
+    "revision": "4c4723f8acbb7fe7b0ce5ea5270dfbcd",
+    "url": "/static/media/alchemy-sprite.4c4723f8.png"
+  },
+  {
+    "revision": "ea6cdcca4528adeee568c1b68dce4862",
+    "url": "/static/media/tailoring-sprite.ea6cdcca.png"
+  },
+  {
+    "revision": "847766d54c20e8b3923cf5b8c51e851e",
+    "url": "/static/media/engineering-sprite.847766d5.png"
+  },
+  {
+    "revision": "ebb087ff7f612a93251a2c6511d8c492",
+    "url": "/static/media/2.ebb087ff.png"
+  },
+  {
+    "revision": "01c58228c87cd4de6e9beb7f1bffe806",
+    "url": "/static/media/9.01c58228.png"
+  },
+  {
+    "revision": "90383826ee5a6c8d1215e4eb3abae534",
+    "url": "/static/media/11.90383826.png"
+  },
+  {
+    "revision": "1939b4bae1edc9a565b5b46a22700c38",
+    "url": "/static/media/12.1939b4ba.png"
+  },
+  {
+    "revision": "878384a747110951cc3ec46ebe538cb7",
+    "url": "/static/media/16.878384a7.png"
+  },
+  {
+    "revision": "56e2564e5cfde421702348c7e41d799d",
+    "url": "/static/media/21.56e2564e.png"
+  },
+  {
+    "revision": "fc952894118b06efed48",
+    "url": "/static/js/main.fc952894.chunk.js"
+  },
+  {
+    "revision": "2f3c460529907194434a08e3e6606cc8",
+    "url": "/static/media/25.2f3c4605.png"
+  },
+  {
+    "revision": "92e989c3857c402753ce113e947d62d0",
+    "url": "/static/media/26.92e989c3.png"
+  },
+  {
+    "revision": "ad5d7a7ca44face3b56f402fdd629123",
+    "url": "/static/media/3.ad5d7a7c.png"
+  },
+  {
+    "revision": "c9c7ddab3c225d566efd7d283fce3639",
+    "url": "/static/media/5.c9c7ddab.png"
+  },
+  {
+    "revision": "d2cd268b2d09693993f7636eb0d47848",
+    "url": "/static/media/4.d2cd268b.png"
+  },
+  {
+    "revision": "6c0841961272b659e9acc38eeac04969",
+    "url": "/static/media/13.6c084196.png"
+  },
+  {
+    "revision": "15d3d190d82c1e7acb6e59661bcdc142",
+    "url": "/static/media/15.15d3d190.png"
+  },
+  {
+    "revision": "4e7477ebe64be5a36d34c604470c7877",
+    "url": "/static/media/17.4e7477eb.png"
+  },
+  {
+    "revision": "15d3d190d82c1e7acb6e59661bcdc142",
+    "url": "/static/media/19.15d3d190.png"
+  },
+  {
+    "revision": "f63f7bad3e13b29a55b45e0dbbd47f54",
+    "url": "/static/media/18.f63f7bad.png"
+  },
+  {
+    "revision": "d6e99e0a8980411be3f41e1317818c1f",
+    "url": "/static/media/22.d6e99e0a.png"
+  },
+  {
+    "revision": "edeb72b0a16162542a124e6360494e94",
+    "url": "/static/media/23.edeb72b0.png"
+  },
+  {
+    "revision": "6131d40baa7aa2564ed3ddc19a47daaf",
+    "url": "/static/media/28.6131d40b.png"
+  },
+  {
+    "revision": "9b731dd4bf81af3e69172b7cb2dbabce",
+    "url": "/static/media/30.9b731dd4.png"
+  },
+  {
+    "revision": "7ca5f188e913eef721a440aa934dfe4c",
+    "url": "/static/media/29.7ca5f188.png"
+  },
+  {
+    "revision": "d4ea80cb77b4e3ba0d9647986be695c9",
+    "url": "/static/media/1.d4ea80cb.png"
+  },
+  {
+    "revision": "2b4e348e96a1b776a1f2afdd207169e0",
+    "url": "/static/media/6.2b4e348e.png"
+  },
+  {
+    "revision": "245c74bce4a877dfa8bd683603fe9afb",
+    "url": "/static/media/7.245c74bc.png"
+  },
+  {
+    "revision": "7f4896e50b8c6a03e36a318b3e3dd03b",
+    "url": "/static/media/8.7f4896e5.png"
+  },
+  {
+    "revision": "d4e96da6a2912940c0056b0cca43b000",
+    "url": "/static/media/10.d4e96da6.png"
+  },
+  {
+    "revision": "78ba83d0102a10121655ff209a84d268",
+    "url": "/static/media/14.78ba83d0.png"
+  },
+  {
+    "revision": "30ff5649dae2ec76f49a05ebb1287443",
+    "url": "/static/media/20.30ff5649.png"
+  },
+  {
+    "revision": "28ea58ab265b03538dc3b4cc861f401b",
+    "url": "/static/media/27.28ea58ab.png"
+  },
+  {
+    "revision": "a0521ea00df74a25e9e6ea4fdf05a127",
+    "url": "/static/media/DUALITY_.a0521ea0.ttf"
+  },
+  {
+    "revision": "a58dd9d7fb4d4497fdf4",
+    "url": "/static/css/1.6e6fd286.chunk.css"
+  },
+  {
+    "revision": "9fe67ac9ea094f6556f5014d8f15db47",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e53ca199da554c5421609bb17632569.js"
+  "/precache-manifest.47ef512a74266bf705e879fce59760f7.js"
 );
 
 workbox.clientsClaim();
